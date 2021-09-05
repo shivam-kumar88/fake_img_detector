6e62a3efbc146258ba78dee8f9915ea8f580c15c
shivam <shivam882531@gmail.com>
2021-09-06 00:12:42 +0530

    some additional file added

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<h1>Status of App</h1>the app is working"
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
