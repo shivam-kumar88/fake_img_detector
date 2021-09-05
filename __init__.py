a4d89e10ad661307c077e2f9d0e2512472c5142a
shivam <shivam882531@gmail.com>
2021-09-05 16:16:28 +0530

    remade the app from scratch

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import flask
-
-app = flask(__name__)
-
-app.config.from_object("config.DevelopmentConfig")
-
-from app import views
--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from app import app
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<h1>Status of App</h1>the app is working"
+
 
 if __name__ == '__main__':
     app.run()
