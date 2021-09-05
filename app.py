a1de04878c672c8478d4c7bc3c88f938067279f2
shivam <shivam882531@gmail.com>
2021-09-06 00:35:55 +0530

    html page added and updated

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<h1>Status of App</h1>the app is working"
-
+    return render_template("index.html")
+    #return ("hellow world")
 
 if __name__ == '__main__':
     app.run()
