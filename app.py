64791bdb49bafeb219f93ef7af4de4649a2bec41
shivam <shivam882531@gmail.com>
2021-09-11 00:52:17 +0530

    able to get link of the picture in terminal

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
@@ -7,10 +7,20 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/upload")
+@app.route("/upload", methods=["POST", "GET"])
 def upload():
-    return "<h1>Upload file</h1>you can upload your filr here"
+    if request.method == "POST":
+        Link = request.form["link"]
+        picture = request.form["url"]
+        print(picture)
+        return redirect(url_for("Link", Lk = Link))
+    else:
+        return render_template("upload.html")
     
+@app.route("/<Lk>")
+def Link(Lk):
+    return f"<h1>{Lk}</h1>"
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
